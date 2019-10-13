bd720efc46cb02a26113b94c542c2be64135b592
Kamil Król <kamil55525@gmail.com>
2019-10-13 14:11:12 +0200

    Add task6

---
+++ list1/task6.jl
@@ -0,0 +1,26 @@
+#author: Kamil Król
+
+using Printf
+
+types = [Float64]
+
+# type - type for which value of function f given in the task should be computed
+# x - argument for which value of function will be computed
+function f(type, x)
+    result = sqrt(x*x + type(1)) - type(1)
+    return result
+end
+
+# type - type for which value of function g given in the task should be computed
+# x - argument for which value of function will be computed
+function g(type, x)
+    result = ( x * x ) / ( sqrt(x * x + type(1)) + type(1) )
+    return result
+end
+
+for n = 1:9
+    x = Float64(8)^(-n)
+    f_ = f(Float64, x)
+    g_ = g(Float64, x)
+    println("For x = 8^(-$n) - $(@sprintf("%.30f",x)):    f = $f_;    g = $g_;   Is it equal? $(f_ == g_)")
+end
