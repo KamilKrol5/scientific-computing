9c3ae08a3c81e7512537bb80f31cd7d7ea644abf
Kamil Król <kamil55525@gmail.com>
2019-10-13 16:26:46 +0200

    Add task4

---
+++ list1/task4.jl
@@ -0,0 +1,19 @@
+#author: Kamil Król
+
+types = [Float64]
+function main()
+    start = nextfloat(Float64(1.0)) #first number in range (1,2)
+    end_ = Float64(2.0) #end of the range (1,2) (exclusive)
+    number = start; #sought number
+
+    while number < end_
+        if number * ( 1.0 / number ) != 1.0
+            break
+        end
+        number = nextfloat(number) #taking next number
+    end
+
+    println("The smallest number x for which x ∗ ( 1.0 / x ) != 1.0 is: $number")
+end
+
+main()
