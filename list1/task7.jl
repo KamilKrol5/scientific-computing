d9a31d8bac7bbf078fba7dbc2efff7c2d6e6e434
Kamil Król <kamil55525@gmail.com>
2019-10-14 22:14:16 +0200

    Add task7

---
+++ list1/task7.jl
@@ -0,0 +1,41 @@
+#author: Kamil Król
+
+using Printf
+
+types = [Float64]
+
+# Computes value of function: sin(x) + cos(3x)
+# type - type for which value of function should be computed
+function analysedFunction(x, type=Float64)
+    return sin(type(x)) + cos(3*type(x))
+    
+end
+
+# Computes approximate value of derivative of function: sin(x) + cos(3x) according to formula given in the task
+# h - argument corresponding to h in the formula
+# x - point in which the derivative value should be computed
+# type - type for which value of derivative should be computed
+function approximationDerivative(h, x, type=Float64)
+    return (analysedFunction(x + h, type) - analysedFunction(x)) / h
+end
+
+
+# Computes derivative of function: sin(x) + cos(3x) according to mathematical formula
+# x - point in which the derivative value should be computed
+# type - type for which value of derivative should be computed
+function derivative(x, type=Float64)
+    # derivative for sin(x) + cos(3x) is: cos(x) - 3 sin(3 x)
+    return cos(type(x)) - 3.0 * sin(3 * type(x))
+end
+
+function main()
+    fromMathFormulaResult = derivative(1.0)
+    println("From mathematical formula: f'(1.0) = $fromMathFormulaResult")
+    for n = 0:54
+        h = 2.0 ^ (-n)
+        approxFormulaResult = approximationDerivative(h, 1.0)
+        println("n = $n; val = $approxFormulaResult; Error: $(abs(fromMathFormulaResult - approxFormulaResult))")
+    end
+end
+
+main()
