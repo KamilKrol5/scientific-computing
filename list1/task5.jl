b82d43c4048e62a8c1c3021e845ff05be1613698
Kamil Król <kamil55525@gmail.com>
2019-10-19 19:07:44 +0200

    Add generating tex table and computing absolute and relative error

--- list1/task5.jl
+++ list1/task5.jl
@@ -69,12 +69,26 @@ scalarProductAscending(x, y, type) = sum( sort(filter(u -> u>0 , multiplyCorresp
 x = [2.718281828, -3.141592654, 1.414213562, 0.5772156649, 0.3010299957]
 y = [1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
 
+realValue = -1.00657107000000e-11
+
 for type in types
+    forward = scalarProductForwardFunctional(x, y, type)
+    backward = scalarProductBackwardFunctional(x, y, type)
+    descending = scalarProductDescending(x, y, type)
+    ascedning = scalarProductAscending(x, y, type)
+    absErrForward = abs(realValue - forward)
+    absErrBackward = abs(realValue - backward)
+    absErrDesc = abs(realValue - descending)
+    absErrAsc = abs(realValue - ascedning)
     println("Type: $type")
-    println("Scalar Product (Forward) = ",scalarProductForwardFunctional(x, y, type))
-    println("Scalar Product (Backward) = ",scalarProductBackwardFunctional(x, y, type))
-    println("Scalar Product (Descending) = ",scalarProductDescending(x, y, type))
-    println("Scalar Product (Ascending) = ",scalarProductAscending(x, y, type))
+    println("Scalar Product (Forward) = ",forward, " absolute error = $absErrForward, relative error = $(abs(absErrForward/realValue))")
+    println("Scalar Product (Backward) = ",backward, " absolute error = $absErrBackward, relative error = $(abs(absErrBackward/realValue))")
+    println("Scalar Product (Descending) = ",descending, " absolute error = $absErrDesc, relative error = $(abs(absErrDesc/realValue))")
+    println("Scalar Product (Ascending) = ",ascedning, " absolute error = $absErrAsc, relative error = $(abs(absErrAsc/realValue))")
+    # println("Forward & $forward & $absErrForward & $(abs(absErrForward/realValue)) \\\\ \\hline")
+    # println("Backward & $backward & $absErrBackward & $(abs(absErrBackward/realValue)) \\\\ \\hline")
+    # println("Descending & $descending & $absErrDesc & $(abs(absErrDesc/realValue)) \\\\ \\hline")
+    # println("Ascending & $ascedning & $absErrAsc & $(abs(absErrAsc/realValue)) \\\\ \\hline")
     println()
 end
 
