67573d86cfa6ea59c7ddd5682b08813981d55ba9
Kamil Król <kamil55525@gmail.com>
2019-10-20 16:49:58 +0200

    Update task 4 and 7

--- list1/task4.jl
+++ list1/task4.jl
@@ -3,18 +3,21 @@
 types = [Float64]
 
 function main()
-    start = nextfloat(Float64(1.0)) #first number in range (1,2)
-    end_ = Float64(2.0) #end of the range (1,2) (exclusive)
-    number = start; #sought number
-
-    while number < end_
-        if number * ( 1.0 / number ) != 1.0
-            break
+    @time begin
+        start = nextfloat(Float64(1.0)) #first number in range (1,2)
+        end_ = Float64(2.0) #end of the range (1,2) (exclusive)
+        number = start; #sought number
+    
+        while number < end_
+            if number * ( 1.0 / number ) != 1.0
+                break
+            end
+            number = nextfloat(number) #taking next number
         end
-        number = nextfloat(number) #taking next number
+    
+        println("The smallest number x for which x ∗ ( 1.0 / x ) != 1.0 is: $(number) ($(bitstring(number)))")
+    end
     end
 
-    println("The smallest number x for which x ∗ ( 1.0 / x ) != 1.0 is: $(number) ($(bitstring(number)))")
-end
 
 main()
