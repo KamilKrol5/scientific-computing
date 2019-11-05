cacd16fa11a4ac7148d066a9d7ac0b43f5cc83f1
Kamil Król <kamil55525@gmail.com>
2019-11-05 15:09:49 +0100

    Add task6

---
+++ list2/task6.jl
@@ -0,0 +1,27 @@
+#author: Kamil Król
+
+function recursion(x_n, c)
+    x_nplusone = x_n * x_n + c
+    return x_nplusone
+end
+
+number_of_iterations = 40
+
+tests = [(-2,1), 
+        (-2,2),
+        (-2,1.99999999999999),
+        (-1, 1),
+        (-1, -1),
+        (-1, 0.75),
+        (-1, 0.25)]
+
+for test in tests
+    c = test[1]
+    x_0 = test[2]
+    result = x_0
+    println("Test: c = $c, x_0 = $x_0")
+    for i = 1:number_of_iterations
+        result = recursion(result, c)
+        println("c = $c, x_0 = $x_0, x_$i = $result")
+    end
+end
