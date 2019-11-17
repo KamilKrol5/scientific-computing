9bfc29013467c9f9da7c554628bab6a238c23504
Kamil Król <kamil55525@gmail.com>
2019-11-17 15:45:09 +0100

    Add task1 and task2

---
+++ list3/task1.jl
@@ -0,0 +1,34 @@
+# author: Kamil Król
+
+# Function which computes zero of given function (f) using bisection method.
+# f - given function,
+# a, b - left and right bound of the range,
+# delta, epsilon - precision of the result.
+function mbisekcji(f, a::Float64, b::Float64, delta::Float64, epsilon::Float64)
+    value_in_left_bound = f(a)
+    value_in_right_bound = f(b)
+    range_length = value_in_right_bound - value_in_left_bound   # the length of range [a,b]
+    iterations = 0
+    if (sign(value_in_left_bound) == sign(value_in_right_bound)) # function does not change its sign in range [a,b]
+        return "error", "error", "error", 1
+    end
+
+    while range_length > epsilon
+        iterations += 1
+        range_length = range_length / 2
+        middle_point_of_range = a + range_length
+        value_in_middle_point = f(middle_point_of_range)    # value of function of in the middle point of range [a,b]
+
+        if (abs(range_length) < delta || abs(value_in_middle_point) < epsilon) # end condition
+            return middle_point_of_range, value_in_middle_point, iterations, 0
+        end
+
+        if (sign(value_in_middle_point) != sign(value_in_left_bound))
+            b = middle_point_of_range
+            value_in_right_bound = value_in_middle_point
+        else
+            a = middle_point_of_range
+            value_in_left_bound = value_in_middle_point
+        end
+    end
+end
---
+++ list3/task2.jl
@@ -0,0 +1,31 @@
+# author: Kamil Król
+
+# Function which computes zero of given function (f) using Newton method.
+# f - given function,
+# f_prim - derivative of given function f,
+# x0 - initial approximation,
+# delta, epsilon - precision of the result,
+# maxit - maxiumum number of iterations.
+function mstycznych(f, f_prim, x0::Float64, delta::Float64, epsilon::Float64, maxit::Int)
+    value_in_x0 = f(x0)
+
+    if(abs(value_in_x0) < epsilon)    # value of function f in x0 is close enough to 0
+        return x0, value_in_x0, 0, 0
+    end
+
+    if (abs(f_prim(x0)) < epsilon)   # value of derivative of function f in point x0 is close to 0
+        return "error", "error", "error", 2
+    end
+
+    for iteration = 1:maxit
+        x1 = x0 - (value_in_x0/f_prim(x0))  # the next approximation of the root
+        value_in_x0 = f(x1)                 # value of function f in the next approximation of the root
+
+        if(abs(x1 - x0) < delta || abs(value_in_x0) < epsilon)  # end condition
+            return x1, value_in_x0, iteration, 0
+        end
+        x0 = x1                             # updating the approximation
+    end
+
+    return "error", "error", "error", 1   # the result was not computed in maxit iterations
+end
